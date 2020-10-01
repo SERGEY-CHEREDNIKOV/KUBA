dbb469ee1ed2b3b85e9b9129699c5099d6b6b6d8
Sergey Cherednikov <serezha.ros@mail.ru>
2020-10-01 11:30:58 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
-$(function(){
+'use strict';
 
-});
+
+const sliders = (slides, dir, prev, next) => {
+  let slideIndex = 1;
+  const items = document.querySelectorAll(slides);
+
+  function showSlides(n) {
+      if (n > items.length) {
+          slideIndex = 1;
+      }
+
+      if (n < 1) {
+          slideIndex = items.length;
+      }
+
+      items.forEach(item => {
+          item.classList.add('animate__animated');
+          item.style.display = 'none';
+      })
+
+      items[slideIndex - 1].style.display = 'block';
+  }
+
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+
+  try {
+      const prevBtn = document.querySelector(prev),
+          nextBtn = document.querySelector(next);
+
+      prevBtn.addEventListener('click', () => {
+          plusSlides(-1);
+          items[slideIndex - 1].classList.remove('animate__slideInLeft');
+          items[slideIndex - 1].classList.add('animate__slideInRight');
+      });
+
+      nextBtn.addEventListener('click', () => {
+          plusSlides(1);
+          items[slideIndex - 1].classList.remove('animate__slideInRight');
+          items[slideIndex - 1].classList.add('animate__slideInLeft');
+      });
+  } catch (e) {
+
+  }
+};
+
+sliders('.slider-item', '', '.slider__button-prev', '.slider__button-next');
